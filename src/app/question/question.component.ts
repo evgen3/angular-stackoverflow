89590a54662652862f6e6ec9685be9af6c9a8d56
Evgenii Kuznetsov <harsel1234@gmail.com>
2020-03-28 14:09:06 +0400

    Добавлен лодер при загрузке ответов

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { tap, map, switchMap } from 'rxjs/operators';
 import { QuestionService, Answer } from './question.service';
+
+const idParamName = 'id';
 
 @Component({
   selector: 'app-question',
@@ -9,17 +12,24 @@ import { QuestionService, Answer } from './question.service';
 })
 export class QuestionComponent implements OnInit {
   answers: Answer[] = [];
+  loading = false;
   constructor(
     private route: ActivatedRoute,
     private questionService: QuestionService
   ) { }
 
   ngOnInit(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-
-    this.questionService.getAnswers(id)
+    this.route.paramMap
+      .pipe(
+        map(params => +params.get(idParamName)),
+        tap(() => {
+          this.loading = true;
+        }),
+        switchMap(id => this.questionService.getAnswers(id))
+      )
       .subscribe(answers => {
         this.answers = answers;
+        this.loading = false;
       });
   }
 }
